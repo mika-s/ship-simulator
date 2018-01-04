ce694b89d779b88319b1ec467d42a98babfd6799
Mika Sundland <mika.sundland@gmail.com>
2018-01-04 12:20:26 +0100

    Added thruster and position pane. Started on map.

---
+++ frontend/src/components/dashboard/Thrusters.pane.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './Dashboard.css';
+
+class ThrustersPane extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    const thrusterElements = this.props.thrusters.map(thruster => (
+      <div className="row" key={thruster.number}>
+        <div className="col-lg-1">
+          {thruster.number}
+        </div>
+        <div className="col-lg-2">
+          {thruster.name}
+        </div>
+        <div className="col-lg-2">
+          {thruster.force.toFixed(1)}
+        </div>
+        <div className="col-lg-2">
+          {thruster.controlType === 'pitch' &&
+            <div>
+              {(thruster.demand.pitch * 100).toFixed(1)}/
+              {(thruster.feedback.pitch * 100).toFixed(1)}
+            </div>}
+        </div>
+        <div className="col-lg-2">
+          {thruster.controlType === 'rpm' &&
+            <div>
+              {(thruster.demand.rpm * 100).toFixed(1)}/
+              {(thruster.feedback.rpm * 100).toFixed(1)}
+            </div>}
+        </div>
+        <div className="col-lg-2">
+          {(thruster.thrusterType === 'propeller' || thruster.thrusterType === 'azimuth') &&
+            <div>
+              {(thruster.demand.azimuth).toFixed(1)}/
+              {(thruster.feedback.azimuth).toFixed(1)}
+            </div>}
+        </div>
+      </div>
+    ));
+
+    return (
+      <div className="thrusterspane">
+        <div className="row" style={{ fontWeight: 'bold', marginBottom: 5 }}>
+          <div className="col-lg-1">#</div>
+          <div className="col-lg-2">Name</div>
+          <div className="col-lg-2">Force [T]</div>
+          <div className="col-lg-2">Pitch [%]</div>
+          <div className="col-lg-2">RPM [%]</div>
+          <div className="col-lg-2">Azimuth [°]</div>
+        </div>
+        {thrusterElements}
+      </div>
+    );
+  }
+}
+
+ThrustersPane.propTypes = {
+  thrusters: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default ThrustersPane;
